fcae0c9a5fd8c422e4dac34cdbb4cc75959a49f2
Drive <ant_ilich@mail.ru>
2020-01-23 21:34:58 +0200

    информация о операционной системе

--- 01_os_info.py
+++ 01_os_info.py
@@ -2,12 +2,12 @@
 
 # Нужно собратть информацию об операционной системе и ыерсии пайтона
 
-# TODO запустить этот скрипт и закомитить результат его рработы (файл os_info.txt)
 
 import platform
 import sys
 
-info = 'OS info is \n {} \n\nPython version is {} {}'.format(platform.uname(), sys.version, platform.architecture())
+info = 'OS info is \n {} \n\nPython version is {} {}'.format(
+    platform.uname(), sys.version, platform.architecture())
 print(info)
 
 with open('os_info.txt', 'w') as ff:
